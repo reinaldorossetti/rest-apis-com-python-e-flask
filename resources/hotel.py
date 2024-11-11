4f1045d928d62d7d5f8328f12e30bb9281d06a66
Reinaldo <reinaldo.rossetti@outlook.com>
2024-11-11 10:24:44 -0300

    adicionando autenticação nos end points

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,32 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
-
-hoteis = [
-    {
-    'hotel_id': "1",
-    'nome': 'Alpha Hotel',
-    'estrelas': 3.0,
-    'diaria': 300.0,
-    'cidade': 'Curitiba',
-    'estado': 'PR'
-    }, 
-    {
-    'hotel_id': "2",
-    'nome': 'Ryan Hotel',
-    'estrelas': 4.0,
-    'diaria': 400.0,
-    'cidade': 'Manaus',
-    'estado': 'AM'
-    },
-    {
-    'hotel_id': "3",
-    'nome': 'Rei Hotel',
-    'estrelas': 5.0,
-    'diaria': 500.0,
-    'cidade': 'Santa Catarina',
-    'estado': 'SC'
-    }
-]
+from flask_jwt_extended import jwt_required
 
 class Hoteis(Resource):
     def get(self):
@@ -35,27 +9,33 @@ class Hoteis(Resource):
 class Hotel(Resource):
 
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('diaria')
+    argumentos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be left brank.")
+    argumentos.add_argument('estrelas', type=str, required=True, help="The field 'nome' cannot be left brank.")
+    argumentos.add_argument('diaria', type=str, required=True, help="The field 'nome' cannot be left brank.")
     argumentos.add_argument('cidade')
     argumentos.add_argument('estado')
 
+    @jwt_required()
     def get(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
             return hotel.json()
         return {'message': 'Hotel not found'}, 404
     
+    @jwt_required()
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
             return {'message': 'Hotel id {} already exists.'.format(hotel_id)}, 400
         
         dados = Hotel.argumentos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to sabe hotel.'}, 500
         return hotel.json(), 200
 
+    @jwt_required()
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
         hotel_encontrado = HotelModel.find_hotel(hotel_id)
@@ -65,12 +45,19 @@ class Hotel(Resource):
             return hotel_encontrado.json(), 200
         
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to sabe hotel.'}, 500
         return  hotel.json(), 201
-
+    
+    @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            hotel.delete_hotel()
+            try:
+                hotel.delete_hotel()
+            except:
+                return  {'message': 'An error ocurred trying to delete hotel.'}, 200 
             return  {'message': 'Hotel deleted'}, 200 
         return  {'message': 'Hotel not found'}, 404
---
+++ resources/usuario.py
@@ -0,0 +1,70 @@
+from flask_restful import Resource, reqparse
+from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+import hmac
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left brank.")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left brank.")
+ 
+
+class User(Resource):
+    # /usuarios/{user_id}
+    def get(self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            return user.json()
+        return {'message': 'User not found.'}, 404
+    
+    @jwt_required()
+    def delete(self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            try:
+                user.delete_user()
+            except:
+                return  {'message': 'An error ocurred trying to delete User.'}, 200 
+            return  {'message': 'User deleted.'}, 200 
+        return  {'message': 'User not found.'}, 404
+
+class UserRegister(Resource):
+    # /cadastro
+    @jwt_required()
+    def post(self):
+        dados = atributos.parse_args()
+        if UserModel.find_by_login(dados['login']):
+            return {'message':"The login '{}' already exists.".format(dados['login'])}
+        
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message':'User created sucessfully.'}, 201     # Created
+
+class UserLogin(Resource):
+
+    @classmethod
+    def safe_str_cmp(cls, a: str, b: str) -> bool:
+        """This function compares strings in somewhat constant time. This
+        requires that the length of at least one string is known in advance.
+
+        Returns `True` if the two strings are equal, or `False` if they are not.
+        """
+        if isinstance(a, str):
+            a = a.encode("utf-8")  # type: ignore
+
+        if isinstance(b, str):
+            b = b.encode("utf-8")  # type: ignore
+
+        return hmac.compare_digest(a, b)
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UserModel.find_by_login(dados['login'])
+        cmp = cls.safe_str_cmp(user.senha, dados['senha'])
+
+        if user and cmp:
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return  {'access_token': token_de_acesso}, 200
+        
+        return {'message': 'The username ou password is incorrect'}, 401
